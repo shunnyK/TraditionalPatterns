e1656f85df8c198b62d4c04ab76019e73e4b9d93
서현 <seohyeon@seohyeon-ui-MacBookPro.local>
2026-06-26 18:52:44 +0900

    Add preview images and debug image loading

--- streamlit_app.py
+++ streamlit_app.py
@@ -329,9 +329,7 @@ if "results" in st.session_state:
         
         st.write(row["file_name"])
         image_path = find_image_path(row["file_name"])
-        st.write("file_name:", row["file_name"])
-        st.write("image_path:", image_path)
-
+       
         col1, col2 = st.columns([1, 2])
 
         with col1:
